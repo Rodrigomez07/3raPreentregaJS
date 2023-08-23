3ae799ec80783a6aac158d0b2a86416a8bc42dd7
Rodrigomez07 <rodrigomez07.rg@gmail.com>
2023-08-23 20:00:01 -0300

    modificar botones carrito

--- carrito.js
+++ carrito.js
@@ -1,37 +1,59 @@
-const productosLs = JSON.parse(localStorage.getItem("productos"))
-const containerCards = document.querySelector("div")
+import { productos } from './productos.js';
 
-const cardsAHtml = array => {
+const productosCarrito = JSON.parse(localStorage.getItem("productos")) || [];
 
-    // generar la lógica que desarrolle las tarjetas (nodos html)
+function showFeedbackMessage(message, type = "success") {
+    Toastify({
+        text: message,
+        className: type,
+        style: {
+            background: type === "success" ? "linear-gradient(to right, #00b09b, #96c93d)" : "#ff4136",
+        }
+    }).showToast();
+}
 
-    const cards = array.reduce((acc, element) => {
-        return acc + `
+function updateCartTotal() {
+    const totalElement = document.querySelector(".cart-total");
+    const total = productosCarrito.reduce((acc, producto) => acc + producto.precio, 0);
+    totalElement.textContent = `Total: $${total.toFixed(2)}`;
+}
+
+function generateCartProductCards(array) {
+    const containerCards = document.querySelector(".container-cards");
+
+    const cards = array.map(element => `
         <div class="card" id="card-${element.id}">
-                  
-            <h2>
-                ${element.producto}
-            </h2>
+            <h2>${element.producto}</h2>
             <figure class="container-card">
                 <img src=${element.imagen || "not-found.jpg"} alt="imagen de ${element.producto}">
             </figure>
-            <h6>
-                Marca: ${element.marca}
-            </h6>
-            <h4>
-                Precio: ${element.precio}
-            </h4>
-            <h6>
-                ${element.descripcion}
-            </h6>
-            
+            <h6>Marca: ${element.marca}</h6>
+            <h4>Precio: ${element.precio}</h4>
+            <button class=".button-remove" data-id="${element.id}">
+                Eliminar
+            </button>
         </div>
-    `
-    }, "")
+    `).join("");
 
-    // generar que se impacten estas tarjetas en el DOM con innerHTML
-    containerCards.innerHTML = cards
+    containerCards.innerHTML = cards;
 
+    const allRemoveButtons = document.querySelectorAll(".button-remove");
+    allRemoveButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            const id = e.currentTarget.getAttribute("data-id");
+            productosCarrito = productosCarrito.filter(producto => producto.id !== Number(id));
+            localStorage.setItem("productos", JSON.stringify(productosCarrito));
+            updateCartTotal();
+
+            // Actualiza la visualización del carrito después de eliminar
+            generateCartProductCards(productosCarrito);
+
+            showFeedbackMessage(`Se ha eliminado un producto del carrito.`);
+        });
+    });
 }
 
-cardsAHtml(productosLs)
+window.addEventListener("load", () => {
+    updateCartTotal();
+    generateCartProductCards(productosCarrito);
+});
--- index.js
+++ index.js
@@ -1,67 +1,63 @@
-console.log(productos)
+import { productos } from './productos.js';
 
-const containerCards = document.querySelector(".container-cards")
+const productosCarrito = JSON.parse(localStorage.getItem("productos")) || [];
 
-const cardsAHtml = array => {
+function showFeedbackMessage(message, type = "success") {
+    Toastify({
+        text: message,
+        className: type,
+        style: {
+            background: type === "success" ? "linear-gradient(to right, #00b09b, #96c93d)" : "#ff4136",
+        }
+    }).showToast();
+}
 
-    // generar la lógica que desarrolle las tarjetas (nodos html)
+function updateCartTotal() {
+    const totalElement = document.querySelector(".cart-total");
+    const total = productosCarrito.reduce((acc, producto) => acc + producto.precio, 0);
+    totalElement.textContent = `Total: $${total.toFixed(2)}`;
+}
 
-    const cards = array.reduce((acc, element) => {
-        return acc + `
+function generateProductCards(array) {
+    const containerCards = document.querySelector(".container-cards");
+
+    const cards = array.map(element => `
         <div class="card" id="card-${element.id}">
-            <button class="button-card" id="button-${element.id}">
-                <i class="fa-solid fa-star"></i>
-            </button>       
-            <h2>
-                ${element.producto}
-            </h2>
+            <h2>${element.producto}</h2>
             <figure class="container-card">
                 <img src=${element.imagen || "not-found.jpg"} alt="imagen de ${element.producto}">
             </figure>
-            <h6>
-                Marca: ${element.marca}
-            </h6>
-            <h4>
-                Precio: ${element.precio}
-            </h4>
-            <h6>
-                ${element.descripcion}
-            </h6>
-            
+            <h6>Marca: ${element.marca}</h6>
+            <h4>Precio: ${element.precio}</h4>
+            <button class="button-card" id="button-${element.id}">
+                Agregar al carrito
+            </button>
+            <h6>${element.descripcion}</h6>
         </div>
-    `
-    }, "")
+    `).join("");
 
-    // generar que se impacten estas tarjetas en el DOM con innerHTML
-    containerCards.innerHTML = cards
+    containerCards.innerHTML = cards;
 
+    const allCards = document.querySelectorAll(".button-card");
+    allCards.forEach(card => {
+        card.addEventListener("click", (e) => {
+            const id = e.currentTarget.id.slice(7);
+            const buscarProducto = array.find(element => element.id === Number(id));
+
+            if (!productosCarrito.some(producto => producto.id === Number(id))) {
+                productosCarrito.push(buscarProducto);
+                localStorage.setItem("productos", JSON.stringify(productosCarrito));
+                showFeedbackMessage(`Se ha añadido ${buscarProducto.producto} al carrito de compras.`);
+                updateCartTotal();
+            } else {
+                showFeedbackMessage(`El producto ya está en el carrito.`, "error");
+            }
+        });
+    });
 }
 
-cardsAHtml(productos)
+function loadInitialProducts() {
+    generateProductCards(productos);
+}
 
-const allCards = document.querySelectorAll(".button-card")
-console.log(allCards)
-
-let productosCarrito = []
-
- const eventoCards = ( nodos, array ) => {
-
-     for ( let i = 0; i < nodos.length; i++ ) {
-
-         nodos[i].onclick = (e) => {                               
-             const id = e.currentTarget.id.slice(7)
-             const buscarProducto = array.find( element => element.id === Number(id))
-             productosCarrito.push(buscarProducto)
-             localStorage.setItem("productos", JSON.stringify(productosCarrito))
-             Toastify({
-                 text: `Se ha añadido  ${ buscarPokemon.name } al carrito de compras.`,
-                 className: "info",
-                 style: {
-                     background: "linear-gradient(to right, #00b09b, #96c93d)",
-                 }
-             }).showToast();
-         }
-     }
- }
-
- eventoCards(allCards, productos)
+window.addEventListener("load", loadInitialProducts);
